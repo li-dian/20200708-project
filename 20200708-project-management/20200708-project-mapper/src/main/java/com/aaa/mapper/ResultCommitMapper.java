4616c6e5355c20e4bddbc6e835ef69651883d1f0
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-16 10:24:05 +0800

    Create ResultCommitMapper.java

---
+++ 20200708-project-management/20200708-project-mapper/src/main/java/com/aaa/mapper/ResultCommitMapper.java
@@ -0,0 +1,12 @@
+package com.aaa.mapper;
+
+import com.aaa.model.ResultCommit;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author LiDian
+ * @createtime 2020/7/15 19:08
+ * @desc
+ **/
+public interface ResultCommitMapper extends Mapper<ResultCommit> {
+}
