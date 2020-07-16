39c629e165020ff59785bf06758daf85abfd479c
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-16 10:23:25 +0800

    Create CheckPersonMapper.java

---
+++ 20200708-project-management/20200708-project-mapper/src/main/java/com/aaa/mapper/CheckPersonMapper.java
@@ -0,0 +1,12 @@
+package com.aaa.mapper;
+
+import com.aaa.model.CheckPerson;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author LiDian
+ * @createtime 2020/7/15 19:07
+ * @desc
+ **/
+public interface CheckPersonMapper extends Mapper<CheckPerson> {
+}
