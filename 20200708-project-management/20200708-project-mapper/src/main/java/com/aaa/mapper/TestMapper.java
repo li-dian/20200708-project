05e9fca0c476075e0839c61ae6aa804c49964abc
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-16 10:24:25 +0800

    Create TestMapper.java

---
+++ 20200708-project-management/20200708-project-mapper/src/main/java/com/aaa/mapper/TestMapper.java
@@ -0,0 +1,12 @@
+package com.aaa.mapper;
+
+import com.aaa.model.Test;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author LiDian
+ * @createtime 2020/7/15 19:09
+ * @desc
+ **/
+public interface TestMapper extends Mapper<Test> {
+}
