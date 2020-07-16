e8e72a598bfbd0a87407039e302967e06f595e41
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-16 10:23:56 +0800

    Create NewsMapper.java

---
+++ 20200708-project-management/20200708-project-mapper/src/main/java/com/aaa/mapper/NewsMapper.java
@@ -0,0 +1,12 @@
+package com.aaa.mapper;
+
+import com.aaa.model.News;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author LiDian
+ * @createtime 2020/7/15 19:08
+ * @desc
+ **/
+public interface NewsMapper extends Mapper<News> {
+}
