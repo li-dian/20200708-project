f66e6864bb64fa5539ea9569760c40974e8d9c92
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-16 10:23:44 +0800

    Create LogMapper.java

---
+++ 20200708-project-management/20200708-project-mapper/src/main/java/com/aaa/mapper/LogMapper.java
@@ -0,0 +1,12 @@
+package com.aaa.mapper;
+
+import com.aaa.model.Log;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author LiDian
+ * @createtime 2020/7/15 19:08
+ * @desc
+ **/
+public interface LogMapper extends Mapper<Log> {
+}
