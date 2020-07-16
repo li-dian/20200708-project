198601c5b1fca163ad7dbe1eadd0778a5da84c87
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-16 10:23:35 +0800

    Create EquipmentMapper.java

---
+++ 20200708-project-management/20200708-project-mapper/src/main/java/com/aaa/mapper/EquipmentMapper.java
@@ -0,0 +1,12 @@
+package com.aaa.mapper;
+
+import com.aaa.model.Equipment;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author LiDian
+ * @createtime 2020/7/15 19:08
+ * @desc
+ **/
+public interface EquipmentMapper extends Mapper<Equipment> {
+}
