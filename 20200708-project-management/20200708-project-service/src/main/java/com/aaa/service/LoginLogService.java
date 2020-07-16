1286381dfa24fd62e73cbf339eb45c70c7349e97
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-16 10:27:13 +0800

    Create LoginLogService.java

---
+++ 20200708-project-management/20200708-project-service/src/main/java/com/aaa/service/LoginLogService.java
@@ -0,0 +1,14 @@
+package com.aaa.service;
+
+import com.aaa.base.BaseService;
+import com.aaa.model.LoginLog;
+import org.springframework.stereotype.Service;
+
+/**
+ * @program: 20200708-project
+ * @author: LiDian
+ * @create: 2020-07-15 20:12:11
+ **/
+@Service
+public class LoginLogService extends BaseService<LoginLog> {
+}
