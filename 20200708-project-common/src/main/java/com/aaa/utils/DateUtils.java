c778f8caff0df659654540c8ef8077b8de7a34cb
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-16 10:20:33 +0800

    Create DateUtils.java

---
+++ 20200708-project-common/src/main/java/com/aaa/utils/DateUtils.java
@@ -0,0 +1,13 @@
+package com.aaa.utils;
+
+/**
+ * @program: 20200708-project
+ * @author: LiDian
+ * @create: 2020-07-10 18:56:30
+ **/
+public class DateUtils {
+
+    private DateUtils() {
+
+    }
+}
