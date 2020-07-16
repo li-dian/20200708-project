d90e68e6b0750c03a916d81049659b5034ca6528
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-16 10:26:03 +0800

    Create Equipment.java

---
+++ 20200708-project-management/20200708-project-model/src/main/java/com/aaa/model/Equipment.java
@@ -0,0 +1,105 @@
+package com.aaa.model;
+import	java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * @author LiDian
+ * @createtime 2020/7/15 15:31
+ * @desc    仪器设备信息表
+ **/
+@Table(name = "t_equipment")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class Equipment implements Serializable{
+    /**
+     * 编号
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * 仪器设备名称
+     */
+    private String name;
+
+    /**
+     * 品牌型号
+     */
+    private String brand;
+
+    /**
+     * 出厂编号
+     */
+    @Column(name = "production_id")
+    private String productionId;
+
+    /**
+     * 数量
+     */
+    private Integer number;
+
+    /**
+     * 检定日期
+     */
+    @Column(name = "check_date")
+    private Date checkDate;
+
+    /**
+     * 检定有效日期
+     */
+    @Column(name = "effective_date")
+    private Date effectiveDate;
+
+    /**
+     * 发票代码
+     */
+    @Column(name = "invoice_code")
+    private String invoiceCode;
+
+    /**
+     * 检定机构
+     */
+    @Column(name = "check_department")
+    private String checkDepartment;
+
+    /**
+     * 检定证书号
+     */
+    @Column(name = "check_certificate_id")
+    private String checkCertificateId;
+
+    /**
+     * 认定
+     */
+    private String identified;
+
+    /**
+     * 单位用户编号
+     */
+    @Column(name = "user_id")
+    private Long userId;
+
+    /**
+     * 创建时间
+
+     */
+    @Column(name = "create_time")
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    @Column(name = "modify_time")
+    private Date modifyTime;
+
+}
