f91f7bd076d2e7c24344cbe41c52121f5cc00c2e
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-16 10:23:21 +0800

    Create AuditMapper.java

---
+++ 20200708-project-management/20200708-project-mapper/src/main/java/com/aaa/mapper/AuditMapper.java
@@ -0,0 +1,12 @@
+package com.aaa.mapper;
+
+import com.aaa.model.Audit;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author LiDian
+ * @createtime 2020/7/15 19:07
+ * @desc
+ **/
+public interface AuditMapper extends Mapper<Audit> {
+}
