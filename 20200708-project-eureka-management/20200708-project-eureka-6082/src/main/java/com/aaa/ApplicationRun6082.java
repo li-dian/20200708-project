dccaee9d94a69d7be0ef8066b338032d1d2bb585
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-16 10:22:37 +0800

    Create ApplicationRun6082.java

---
+++ 20200708-project-eureka-management/20200708-project-eureka-6082/src/main/java/com/aaa/ApplicationRun6082.java
@@ -0,0 +1,16 @@
+package com.aaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: 20200708-project
+ * @author: LiDian
+ * @create: 2020-07-16 08:47:18
+ **/
+@SpringBootApplication
+public class ApplicationRun6082 {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRun6082.class, args);
+    }
+}
