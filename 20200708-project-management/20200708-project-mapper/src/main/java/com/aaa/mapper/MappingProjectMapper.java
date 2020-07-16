71b1d76cab83ddb6a4b06a9ad0e95f04716400d5
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-16 10:23:47 +0800

    Create MappingProjectMapper.java

---
+++ 20200708-project-management/20200708-project-mapper/src/main/java/com/aaa/mapper/MappingProjectMapper.java
@@ -0,0 +1,12 @@
+package com.aaa.mapper;
+
+import com.aaa.model.MappingProject;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author LiDian
+ * @createtime 2020/7/15 19:08
+ * @desc
+ **/
+public interface MappingProjectMapper extends Mapper<MappingProject> {
+}
