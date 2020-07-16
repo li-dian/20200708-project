2afcc9d5f72eb525b0f8d4cf77546265c6295ebc
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-16 10:26:36 +0800

    Create Role.java

---
+++ 20200708-project-management/20200708-project-model/src/main/java/com/aaa/model/Role.java
@@ -0,0 +1,55 @@
+package com.aaa.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author LiDian
+ * @createtime 2020/7/15 15:47
+ * @desc    角色表
+ **/
+@Table(name = "t_role")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class Role implements Serializable {
+    /**
+     * 角色ID
+     */
+    @Id
+    @Column(name = "ROLE_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roleId;
+
+    /**
+     * 角色名称
+     */
+    @Column(name = "ROLE_NAME")
+    private String roleName;
+
+    /**
+     * 角色描述
+     */
+    @Column(name = "REMARK")
+    private String remark;
+
+    /**
+     * 创建时间
+     */
+    @Column(name = "CREATE_TIME")
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    @Column(name = "MODIFY_TIME")
+    private Date modifyTime;
+
+}
