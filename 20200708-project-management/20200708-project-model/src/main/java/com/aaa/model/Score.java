52ce611a1f28dd5792cf2a162d9d5bb3d1519839
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-16 10:26:42 +0800

    Create Score.java

---
+++ 20200708-project-management/20200708-project-model/src/main/java/com/aaa/model/Score.java
@@ -0,0 +1,70 @@
+package com.aaa.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author LiDian
+ * @createtime 2020/7/15 15:49
+ * @desc    单位分值表
+ **/
+@Table(name = "t_score")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class Score implements Serializable {
+    /**
+     * 编号
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * 增加的分值
+     */
+    @Column(name = "score_plus")
+    private Integer scorePlus;
+
+    /**
+     * 减少的分值
+     */
+    @Column(name = "score_subtract")
+    private Integer scoreSubtract;
+
+    /**
+     * 当前分值
+     */
+    private Integer score;
+
+    /**
+     * 关联单位编号
+     */
+    @Column(name = "unit_id")
+    private Long unitId;
+
+    /**
+     * 增加/减少分值的原因
+     */
+    private String reason;
+
+    /**
+     * 创建时间
+     */
+    @Column(name = "create_time")
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    @Column(name = "modify_time")
+    private Date modifyTime;
+
+}
