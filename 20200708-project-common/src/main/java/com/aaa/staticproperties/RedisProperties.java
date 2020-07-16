33a7fac91c57c1a315fdf427b08cb4dc6f93fbb1
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-16 10:20:17 +0800

    Create RedisProperties.java

---
+++ 20200708-project-common/src/main/java/com/aaa/staticproperties/RedisProperties.java
@@ -0,0 +1,16 @@
+package com.aaa.staticproperties;
+
+/**
+ * @program: 20200708-project
+ * @author: LiDian
+ * @create: 2020-07-10 18:52:11
+ **/
+public class RedisProperties {
+
+    public static final String NX = "nx";
+    public static final String XX = "xx";
+    public static final String PX = "px";
+    public static final String EX = "ex";
+    public static final String NO = "NO";
+    public static final String POINT = ".";
+}
