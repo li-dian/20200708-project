9ee5d27ffc872c613d26c30316cacc9b03683541
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-16 10:20:25 +0800

    Update LoginStatus.java

--- 20200708-project-common/src/main/java/com/aaa/status/LoginStatus.java
+++ 20200708-project-common/src/main/java/com/aaa/status/LoginStatus.java
@@ -1,19 +1,23 @@
 package com.aaa.status;
 
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
 /**
- * @Company AAA软件教育
  * @Author LiDian
  * @Date Create in 2020/7/8 11:14
  * @Description
  **/
 public enum LoginStatus {
-
+    /**
+     * 登录状态码
+     */
     LOGIN_SUCCESS("200", "登录成功"),
     LOGIN_FAILED("400", "登录失败，系统异常"),
     USER_EXIST("201", "用户已经存在"),
     USER_NOT_EXIST("401", "用户不存在"),
     PASSWORD_WRONG("402", "密码错误"),
-    LOGOUT_WRONG("405", "用户退出异常");
+    LOGOUT_WRONG("405", "用户退出异常"),
+    SYSTEM_EXCEPTION("406","系统异常");
 
     LoginStatus(String code, String msg) {
         this.code = code;
