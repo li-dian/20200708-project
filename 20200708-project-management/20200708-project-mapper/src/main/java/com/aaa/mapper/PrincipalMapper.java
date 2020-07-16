af8fe198b0e0d79d3912989839c8931393322fdc
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-16 10:24:00 +0800

    Create PrincipalMapper.java

---
+++ 20200708-project-management/20200708-project-mapper/src/main/java/com/aaa/mapper/PrincipalMapper.java
@@ -0,0 +1,12 @@
+package com.aaa.mapper;
+
+import com.aaa.model.Principal;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author LiDian
+ * @createtime 2020/7/15 19:08
+ * @desc
+ **/
+public interface PrincipalMapper extends Mapper<Principal> {
+}
