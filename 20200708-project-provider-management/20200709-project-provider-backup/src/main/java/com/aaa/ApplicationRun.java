492351ea0639d465a490232e7df542e87b1099f9
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-16 10:27:31 +0800

    Create ApplicationRun.java

---
+++ 20200708-project-provider-management/20200709-project-provider-backup/src/main/java/com/aaa/ApplicationRun.java
@@ -0,0 +1,13 @@
+package com.aaa;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * @program: 20200708-project
+ * @author: LiDian
+ * @create: 2020-07-13 10:37:51
+ **/
+@SpringBootApplication
+public class ApplicationRun {
+}
