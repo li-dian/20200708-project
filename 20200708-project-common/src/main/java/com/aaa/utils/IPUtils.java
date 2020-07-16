fa7e63f509027c09e026eb749189e21c29c13dea
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-16 10:20:43 +0800

    Create IPUtils.java

---
+++ 20200708-project-common/src/main/java/com/aaa/utils/IPUtils.java
@@ -0,0 +1,27 @@
+package com.aaa.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @program: 20200708-project
+ * @author: LiDian
+ * @create: 2020-07-15 19:29:21
+ **/
+public class IPUtils {
+
+    private static final String UNKNOWN = "unknown";
+
+    public static String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return "0:0:0:0:0:0:0:1".equals(ip) ? "127.0.0.1" : ip;
+    }
+}
