646ebe70f14ac39665804ba065f37672562d44b2
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-16 10:27:33 +0800

    Update CommonController.java

--- 20200708-project-provider-management/20200709-project-provider-backup/src/main/java/com/aaa/base/CommonController.java
+++ 20200708-project-provider-management/20200709-project-provider-backup/src/main/java/com/aaa/base/CommonController.java
@@ -1,9 +1,16 @@
 package com.aaa.base;
 
+import com.github.pagehelper.PageInfo;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -45,6 +52,10 @@ public abstract class CommonController<T> extends BaseController {
         //TODO AddMethod After to do something
     }
 
+    /**
+     *      调用BaseService
+     * @return
+     */
     public abstract BaseService<T> getBaseService();
 
 
@@ -97,5 +108,109 @@ public abstract class CommonController<T> extends BaseController {
             return super.operationSuccess();
         }
         return super.operationFailed();
+    }
+
+    /**
+     * 更新操作
+     * @param map
+     * @return
+     */
+    public ResultData update(@RequestBody Map map){
+        T t = getBaseService().newInstance(map);
+        int updateResult = getBaseService().update(t);
+        if(updateResult > 0){
+            return  operationSuccess();
+        }
+        return operationFailed();
+    }
+
+    /**
+     * 查询一条数据
+     * @param map
+     * @return
+     */
+    public ResultData getOne(@RequestBody Map map) {
+        T t = getBaseService().newInstance(map);
+        t = getBaseService().selectOne(t);
+        if (null != t) {
+            return operationSuccess(t);
+        }
+        return operationFailed();
+    }
+
+    /**
+     * 条件查询多条结果
+     * @param map
+     * @return
+     */
+    public ResultData getList(@RequestBody Map map){
+        T t = getBaseService().newInstance(map);
+        List<T> resultT = getBaseService().selectList(t);
+        if(resultT.size() > 0) {
+            return operationSuccess(resultT);
+        }
+        return operationFailed("未找到查询结果");
+    }
+
+    /**
+     * 带条件的分页查询
+     * @param map
+     * @param pageNo
+     * @param pageSize
+     * @return
+     */
+    public ResultData getListByPage(@RequestBody Map map, @RequestParam("pageNo") int pageNo, @RequestParam("pageSize") int pageSize){
+        T t = getBaseService().newInstance(map);
+        PageInfo<T> pageInfo = getBaseService().selectListByPage(t,pageNo,pageSize);
+        List<T> resultList = pageInfo.getList();
+        if(resultList.size() > 0) {
+            return operationSuccess(pageInfo);
+        }
+        return operationFailed("未找到查询结果");
+    }
+
+    /**
+     * 不带条件的分页查询
+     * @param pageNo
+     * @param pageSize
+     * @return
+     */
+    public ResultData getListByPage( @RequestParam("pageNo") int pageNo, @RequestParam("pageSize") int pageSize){
+        PageInfo<T> pageInfo =getBaseService().selectListByPage(null,pageNo,pageSize);
+        List<T> resultList = pageInfo.getList();
+        if(resultList.size() > 0) {
+            return operationSuccess(pageInfo);
+        }
+        return operationFailed("未找到查询结果");
+    }
+
+    /**
+     * 从本地当前线程中获取request对象
+     * @return
+     */
+    public HttpServletRequest getServletRequest() {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        ServletRequestAttributes servletRequestAttributes;
+        if (requestAttributes instanceof ServletRequestAttributes) {
+            servletRequestAttributes = (ServletRequestAttributes) requestAttributes;
+            return servletRequestAttributes.getRequest();
+        }
+        return null;
+    }
+
+    /**
+     * 获取当前客户端session对象(如果没有则创建一个新的session)
+     * @return
+     */
+    public HttpSession getSession() {
+        return getServletRequest().getSession();
+    }
+
+    /**
+     * 获取当前客户端session对象(如果没有则直接返回null)
+     * @return
+     */
+    public HttpSession getExistSession() {
+        return getServletRequest().getSession(false);
     }
 }
