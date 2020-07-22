d680c9f9df39ffc59923671c6c02e0f87c98146d
li-dian <67729489+li-dian@users.noreply.github.com>
2020-07-22 19:02:10 +0800

    Create TimeFormatProperties.java

---
+++ 20200708-project-common/src/main/java/com/aaa/staticproperties/TimeFormatProperties.java
@@ -0,0 +1,13 @@
+package com.aaa.staticproperties;
+
+/**
+ * @program: 20200708-project
+ * @author: LiDian
+ * @create: 2020-07-10 18:54:18
+ **/
+public class TimeFormatProperties {
+
+    public static final String DATE_FORMAT = "yyyy/MM/dd";
+
+    public static final String TIME_FORMAT = "yyyy/MM/dd hh:mm:ss";
+}
